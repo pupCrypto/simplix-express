3285b7f2423b9656748173df9c3f290033d216f4
pupCrypto <ujif_cryptoriders@mail.ru>
2025-01-25 16:14:00 +0600

    minors"

--- src/router.js
+++ src/router.js
@@ -1,8 +1,32 @@
-const express = require('express');
-const { SimplixExpress } = require('./simplix');
+const { Router: RouterExpress } = require('express');
 
 class Router {
-    _wrap = SimplixExpress.prototype._wrap;
+    constructor() {
+        this._router = RouterExpress();
+        this._routes = [];
+    }
+
+    get(path, callback) {
+        this._routes.push({ method: 'get', path, callback });
+        this._router.get(path, this._wrap(callback));
+    }
+    post(path, callback) {
+        this._routes.push({ method: 'post', path, callback });
+        this._router.post(path, this._wrap(callback));
+    }
+
+    _wrap(callback) {
+        return async (req, res, next) => {
+            generalContext.attach({ request: req, response: res });
+            try {
+                var result = await callback();
+            } catch(e) {
+                return res.status(500).send('Internal Server Error');
+            }
+            generalContext.clear();
+            return res.send(result.toString());
+        }
+    }
 }
 
 module.exports = {
